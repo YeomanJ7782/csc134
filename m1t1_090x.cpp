2d27716c129d6f25234696d86b669514fb13e73a
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-09-02 18:18:24 +0000

    finished m1t1

--- m1t1_090x.cpp
+++ m1t1_090x.cpp
@@ -5,3 +5,19 @@ Julie Y
 9/2/25
 Program will greet the user.
 */
+
+// magic words
+#include <iostream>
+using namespace std;
+
+int main() {
+    // input: start the program
+    // processing: none
+    // output: 0 (no errors)
+    // special FX: Says "Hello" to the user.
+
+    // Let's say hello
+    cout << "Hello, user!" << endl; // "end line" not "end 1"
+    
+   return 0; // no errors 
+}
