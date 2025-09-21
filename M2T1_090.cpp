9bb527e50da62638da4bc6105678f424ca685617
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-09-21 21:53:46 +0000

    Started M2T1

---
+++ M2T1_090.cpp
@@ -0,0 +1,34 @@
+// CSC 134
+// M2T1 - Product Sales
+// Julie Yeoman
+// 9/21/2025
+// Making a simple "checkout" machine
+
+#include <iostream>
+using namespace std;
+
+int main()
+
+    // Set up variables
+    string first_name, last_name, full_name; // holds customer name
+    string product = "cakes"; // changed to cakes
+    double cost_each = 0.99;
+    double total_cost;
+
+    // Greet the customer
+    cout << "Welcome to our " << product << " store!" << endl;
+    cout << "What is your first name? ";
+    cin >> first_name;
+    cout << "What is your last name? ";
+    cin >> last_name;
+    full_name = first_name + " " + last_name;
+    cout << "Nice to meet you, " << full_name << endl;
+
+    
+    // Ash how much they want to purchase
+
+    // Calculate total price
+
+    // Give result
+
+    return 0; // no errors
