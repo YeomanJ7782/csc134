5d91a1ab8a8ac5058ae13004c72f7f9152939d39
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-09-21 22:19:15 +0000

    Finished M2T1

--- M2T1_090.cpp
+++ M2T1_090.cpp
@@ -5,15 +5,18 @@
 // Making a simple "checkout" machine
 
 #include <iostream>
+#include <iomanip> // for the 2 decimal places
 using namespace std;
 
 int main()
+{
 
     // Set up variables
     string first_name, last_name, full_name; // holds customer name
-    string product = "cakes"; // changed to cakes
-    double cost_each = 0.99;
+    string product = "cake"; // changed to cake
+    double cost_each = 9.99;
     double total_cost;
+    int amount_purchased;
 
     // Greet the customer
     cout << "Welcome to our " << product << " store!" << endl;
@@ -26,9 +29,20 @@ int main()
 
     
     // Ash how much they want to purchase
+    cout << "How many " << product << " would you like today? ";
+    cin >> amount_purchased;
 
     // Calculate total price
+    total_cost = amount_purchased * cost_each;
+
+    // Formatting: Set all prices to 2 decimal places
+    cout << setprecision(2) << fixed;
 
     // Give result
+    cout << "For " << amount_purchased << " " << product << endl;
+    cout << "That will be: $" << total_cost << endl;
+    cout << "Thank you for shopping with us!" << endl;
 
+    
     return 0; // no errors
+}
