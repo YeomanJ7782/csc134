5e7d3d43d9e35f02f1bbb2b3fdb48c85ec8bce1a
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-09-27 02:33:55 +0000

    started M2HW question 3

---
+++ M2HWQ3.cpp
@@ -0,0 +1,37 @@
+/*
+CSC 134
+M2HW Question 3
+Julie Yeoman
+September 26 2025
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int pizzas, slicesPerPizza, visitors;
+    int totalSlices, neededSlices, leftovers;
+
+    // Input
+    cout << "Enter the number of pizzas ordered: ";
+    cin >> pizzas;
+
+    cout << "Enter the number of slices per pizza: ";
+    cin >> slicesPerPizza;
+
+    cout << "Enter the number of visitors: ";
+    cin >> visitors;
+
+    // Calculations
+    totalSlices = pizzas * slicesPerPizza;
+    neededSlices = visitors * 3;
+    leftovers = totalSlices - neededSlices;
+
+    // Output
+    cout << "\n--- Pizza Party Report ---\n";
+    cout << "Total slices available: " << totalSlices << endl;
+    cout << "Slices needed (3 per visitor): " << neededSlices << endl;
+    cout << "Slices left over: " << leftovers << endl;
+
+    return 0;
+}
