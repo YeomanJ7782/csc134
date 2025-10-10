501b87601e08c73fcfd2a67218274e9734bf2150
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-10-10 02:31:31 +0000

    finished M3T1

--- M3T1.cpp
+++ M3T1.cpp
@@ -10,19 +10,26 @@ using namespace std;
 
 int main() {
     // declare variables
-    int widthOne, lengthOne;
-    int areaOne;
+    int widthOne, lengthOne, widthTwo, lengthTwo;
+    int areaOne, areaTwo;
 
     // ask for user input
-    cout << "Please enter the width and height of the first\n"
+    cout << "Please enter the width and height of the first" << endl;
     cout << "rectangle, separated by a space or newline" << endl;
     cin >> widthOne >> lengthOne;
 
+    cout << "Please enter the width and height of the first" << endl;
+    cout << "rectangle, separated by a space or newline" << endl;
+    cin >> widthTwo >> lengthTwo;
+
+
     // calculate answer
     areaOne = widthOne * lengthOne;
+    areaTwo = widthTwo * lengthTwo;
 
 
     // display results
-    cout << "Rectangle has area of " << areaOne << endl;
+    cout << "Rectangle one has area of " << areaOne << endl;
+    cout << "Rectangle two has area of " << areaTwo << endl;
 
 }
