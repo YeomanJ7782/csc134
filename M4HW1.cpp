8000090e17c5a5d36c0e2581aff06977c7c1f9d1
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-10-29 02:56:28 +0000

    finished m4hw1

--- M4HW1.cpp
+++ M4HW1.cpp
@@ -5,3 +5,27 @@ Julie Yeoman
 10/28/25
 */
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    // Ask until valid input
+    cout << "Enter a number from 1 to 12: ";
+    cin >> num;
+
+    while (num < 1 || num > 12) {
+        cout << "Invalid input. Please enter a number between 1 and 12: ";
+        cin >> num;
+    }
+
+    // Print times table
+    int i = 1;
+    while (i <= 12) {
+        cout << num << " times " << i << " is " << num * i << "." << endl;
+        i++;
+    }
+
+    return 0;
+}
