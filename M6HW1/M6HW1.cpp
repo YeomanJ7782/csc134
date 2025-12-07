c253f025181beb8dc622a5731d5b264b1686f66f
Julie Y <yeomanj7782@student.faytechcc.edu>
2025-12-07 09:41:47 +0000

    updated m6hw1

--- M6HW1/M6HW1.cpp
+++ M6HW1/M6HW1.cpp
@@ -7,6 +7,7 @@ Julie Yeoman
 
 #include <iostream>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 // Direction constants (enum)
@@ -15,6 +16,12 @@ const string DIRECTION_NAMES[NUM_DIRECTIONS] = { "north", "east", "south", "west
 
 // Room constants (enum)
 enum Room { ENTRYWAY = 0, LAVENDER_ROOM, PEACH_KITCHEN, BLOOM_GARDEN, ROSE_CELLAR, TREASURE_ROOM, NUM_ROOMS };
+
+// Helper function to make string lowercase
+string toLower(string str) {
+    transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
 
 int main() {
     string roomNames[NUM_ROOMS] = {
@@ -41,7 +48,7 @@ int main() {
         "a cup of peach tea",
         "a soft pink flower",
         "a small rose quartz stone",
-        "a golden key"
+        ""
     };
 
     bool itemTaken[NUM_ROOMS] = { false, false, false, false, false, false };
@@ -71,11 +78,17 @@ int main() {
     int currentRoom = ENTRYWAY;
     bool running = true;
 
+    // Clear game instructions
+    cout << "Welcome to the Pastel Cottage Adventure!\n";
+    cout << "Your goal is to find the hidden treasure in the cottage.\n";
+    cout << "To win, you must explore the rooms, collect important items, and unlock the Treasure Room.\n";
+    cout << "Type 'help' at any time to see available commands.\n";
+
     while (running) {
-        cout << "\nYou are in the " << roomNames[currentRoom] << ".\n";
+        cout << "\n--- " << roomNames[currentRoom] << " ---\n";
         cout << roomDescriptions[currentRoom] << "\n";
 
-        if (!itemTaken[currentRoom])
+        if (!itemTaken[currentRoom] && roomItems[currentRoom] != "")
             cout << "You see " << roomItems[currentRoom] << " here.\n";
 
         cout << "Exits: ";
@@ -88,14 +101,28 @@ int main() {
         if (!printed) cout << "none";
         cout << "\n";
 
+        // Special reminder for key rooms
+        if (currentRoom == LAVENDER_ROOM && !itemTaken[ENTRYWAY])
+            cout << "Remember: you may need items from other rooms to unlock new doors!\n";
+
         cout << "\nWhat would you like to do? ";
         string command;
-        cin >> command;
+        getline(cin, command);
+        command = toLower(command);
 
         if (command == "n") command = "north";
         if (command == "e") command = "east";
         if (command == "s") command = "south";
         if (command == "w") command = "west";
+
+        if (command == "help") {
+            cout << "\nAvailable commands:\n";
+            cout << "- north, south, east, west (or n, s, e, w) to move\n";
+            cout << "- take (to pick up an item in the room)\n";
+            cout << "- inventory (to check your items)\n";
+            cout << "- quit (to exit the game)\n";
+            continue;
+        }
 
         // Handle movement
         bool moved = false;
@@ -103,14 +130,13 @@ int main() {
             if (command == DIRECTION_NAMES[d]) {
                 int nextRoom = connections[currentRoom][d];
 
-                // Lock-and-key: Treasure Room requires golden key
                 if (nextRoom == TREASURE_ROOM) {
                     bool hasKey = false;
                     for (int i = 0; i < inventoryCount; i++)
                         if (inventory[i] == "a pastel keychain") hasKey = true;
 
                     if (!hasKey) {
-                        cout << "The door to the Treasure Room is locked! You need a key.\n";
+                        cout << "The door to the Treasure Room is locked! You need a pastel keychain to enter.\n";
                         moved = true;
                         break;
                     } else {
@@ -122,29 +148,30 @@ int main() {
                 else cout << "You can't go that way.\n";
 
                 moved = true;
+
+                if (currentRoom == TREASURE_ROOM) {
+                    cout << "\n🎉 Congratulations! You've found the treasure and won the game! 🎉\n";
+                    running = false;
+                }
                 break;
             }
         }
-        if (moved) {
-            // Gold tier ending
-            if (currentRoom == TREASURE_ROOM) {
-                cout << "\nCongratulations! You've found the treasure and completed the game!\n";
-                running = false;
+        if (moved) continue;
+
+        if (command == "take") {
+            if (!itemTaken[currentRoom] && roomItems[currentRoom] != "") {
+                cout << "You pick up " << roomItems[currentRoom] << ".\n";
+                inventory[inventoryCount++] = roomItems[currentRoom];
+                itemTaken[currentRoom] = true;
+
+                cout << "Your inventory now contains:\n";
+                for (int i = 0; i < inventoryCount; i++) cout << "- " << inventory[i] << "\n";
+            } else {
+                cout << "There is nothing to take here.\n";
             }
             continue;
         }
 
-        // Take item
-        if (command == "take") {
-            if (!itemTaken[currentRoom]) {
-                cout << "You pick up " << roomItems[currentRoom] << ".\n";
-                inventory[inventoryCount++] = roomItems[currentRoom];
-                itemTaken[currentRoom] = true;
-            } else cout << "There is nothing to take.\n";
-            continue;
-        }
-
-        // Show inventory
         if (command == "inventory") {
             cout << "\nYou are carrying:\n";
             if (inventoryCount == 0) cout << "Nothing yet.\n";
@@ -153,13 +180,14 @@ int main() {
         }
 
         if (command == "quit") {
+            cout << "Thanks for playing! Goodbye.\n";
             running = false;
             continue;
         }
 
-        cout << "I don't understand that command.\n";
+        cout << "I don't understand that command. Type 'help' for a list of commands.\n";
     }
 
-    cout << "\nThank you for exploring this pastel cottage!\n";
     return 0;
 }
+
